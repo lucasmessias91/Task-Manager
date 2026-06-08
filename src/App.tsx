7b9f19df0186d234c9c92eb7e71cb48952d1b8cf
lucasmessias91 <lmessias91@gmail.com>
2026-06-07 21:58:46 -0300

    fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Header } from './components/Header';
+import { TaskForm } from './components/TaskForm';
+import { TaskList } from './components/TaskList';
+import './styles/theme.css';
+import './styles/global.css';
+import { Container } from './components/Container';
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Tarefa = {
+  id: string;
+  texto: string;
+  concluida: boolean;
+};
+
+export function App() {
+  const nomeUsuario = 'Rafael';
+
+  const [tarefas, setTarefas] = useState<Tarefa[]>(() => {
+    const tarefasSalvas = localStorage.getItem('@taskmanager:tarefas');
+    if (tarefasSalvas) {
+      return JSON.parse(tarefasSalvas);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@taskmanager:tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  const quantidadeTarefasPendentes = tarefas.filter(t => !t.concluida).length;
+
+  function handleAddTarefa(tarefa: string) {
+    const newTarefas = [
+      ...tarefas,
+      {
+        id: crypto.randomUUID(),
+        texto: tarefa,
+        concluida: false,
+      },
+    ];
+    setTarefas(newTarefas);
+  }
+
+  function handleToggleTask(id: string) {
+    setTarefas(
+      tarefas.map(t => (t.id === id ? { ...t, concluida: !t.concluida } : t)),
+    );
+  }
+
+  function handleDeleteTask(id: string) {
+    setTarefas(tarefas.filter(t => t.id !== id));
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <Container>
+        <Header
+          quantidadeTarefas={quantidadeTarefasPendentes}
+          nomeUsuario={nomeUsuario}
+        />
+        <TaskForm adicionartarefa={handleAddTarefa} />
+        <TaskList
+          tarefas={tarefas}
+          onToggleTask={handleToggleTask}
+          onDeleteTask={handleDeleteTask}
+        />
+      </Container>
     </>
-  )
+  );
 }
-
-export default App
